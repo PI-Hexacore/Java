2317f061c85f23f69bc88d2f7ca4f5153c58b98c
WillianMarti <willian.souza@sptech.school>
2025-10-31 20:35:48 -0300

    Atualizando credenciais aws

--- Hexacore-backend-java/src/main/java/school/sptech/S3Provider.java
+++ Hexacore-backend-java/src/main/java/school/sptech/S3Provider.java
@@ -1,22 +1,21 @@
 package school.sptech;
 
-import software.amazon.awssdk.auth.credentials.AwsBasicCredentials;
-import software.amazon.awssdk.auth.credentials.ProfileCredentialsProvider;
-import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider;
+import software.amazon.awssdk.auth.credentials.AwsCredentialsProvider;
+import software.amazon.awssdk.auth.credentials.DefaultCredentialsProvider;
 import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.s3.S3Client;
 
 public class S3Provider {
+    private final AwsCredentialsProvider credentials;
+
+    public S3Provider() {
+        this.credentials = DefaultCredentialsProvider.create();
+    }
 
     public S3Client getS3Client() {
-        AwsBasicCredentials awsCreds = AwsBasicCredentials.create(
-                "ASIA3HSRJ576ZUSTCMJ2",
-                "hNyrUQQxJkfwS21U8cYW9g3HBbHYgkiGF97fLBKG"
-        );
-
         return S3Client.builder()
-                .region(Region.US_EAST_1) // ajuste para a região do seu bucket
-                .credentialsProvider(StaticCredentialsProvider.create(awsCreds))
+                .region(Region.US_EAST_1)
+                .credentialsProvider(credentials)
                 .build();
     }
 }
